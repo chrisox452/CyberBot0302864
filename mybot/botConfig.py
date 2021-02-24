7550083b2177da5bb1237acd43cff3598d8eb813
Chris <51244005+chrisox452@users.noreply.github.com>
2021-02-24 23:59:43 +0000

    Add files via upload

---
+++ mybot/botConfig.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+
+##Chatbot customisation
+myBotName = "CyberBot"
+botTimeZone = "Europe/London"
+botAvatar = "/static/placeholderImage.jpg" ##Image will be replaced by virtual agent
+chatBG = "/static/bg.jpg"  ##Browser background
+useGoogle = "yes" ## yes or no if you want Google links provided when your Bot does not have an answer
+confidenceLevel = 0.65 ##Bot confidence level - must be betwee. 0.00 and 1
